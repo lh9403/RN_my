76ba2e2c8059a67eeb9b6da31c5b2a403ab2584b
lh <515026722@qq.com>
2020-05-25 18:26:11 +0800

    rn项目初始化

---
+++ ios/Pods/Target Support Files/Pods-RN_my-RN_myTests/Pods-RN_my-RN_myTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RN_my_RN_myTests : NSObject
+@end
+@implementation PodsDummy_Pods_RN_my_RN_myTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-RN_my-tvOS/Pods-RN_my-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RN_my_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_RN_my_tvOS
+@end
